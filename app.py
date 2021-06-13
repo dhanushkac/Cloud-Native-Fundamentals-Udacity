239d45dae96e292d82afb04f7780e6a85bf8abb0
Dhanushka C <dhanushka787@gmail.com>
2021-06-13 23:29:42 +0530

    Stage #2 - logging 🏴󠁡󠁦󠁬󠁯󠁧󠁿

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+from datetime import datetime
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 
 @app.route("/status")
 def status():
     res = {"result": "OK - healthy"}
+    log()
     return make_response(jsonify(res), 200)
 
 
 @app.route("/metrics")
 def metrics():
     res = {"data": {"UserCount": 140, "UserCountActive": 23}}
+    log()
     return make_response(jsonify(res), 200)
 
 
 @app.route("/")
 def hello():
+    log()
     return "Hello World"
+
+
+def log():
+    now = datetime.now()
+    timestamp = int(datetime.timestamp(now))
+
+    app.logger.info('%s, %s endpoint was reached', timestamp, request.path)
 
 
 if __name__ == "__main__":
